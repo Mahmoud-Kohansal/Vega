7e50e589a4b00b0ac6e22607e63edc986a88d572
kohansal6396 <mahmoud.kohansal@gmail.com>
2021-03-17 11:07:24 +0330

    Mapping Collection
    Inserting duplicate Key of ICollection get error,
    So in mapping profile Map Collection correctly.

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using System.Xml.XPath;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Vega.Controllers.Resources;
 using Vega.Models;
 using Vega.Persistence;
@@ -31,6 +32,23 @@ namespace Vega.Controllers
             Vehicle vehicle = this.mapper.Map<VehicleResource, Vehicle>(vehicleResource);
             vehicle.LastUpdate = DateTime.Now;
             dbContext.Vehicles.Add(vehicle);
+            await dbContext.SaveChangesAsync();
+
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+
+            return Ok(result);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateVehicle(int id, [FromBody] VehicleResource vehicleResource)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            Vehicle vehicle = await dbContext.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id );
+            this.mapper.Map<VehicleResource, Vehicle>(vehicleResource, vehicle);
+            vehicle.LastUpdate = DateTime.Now;
+            dbContext.Vehicles.Update(vehicle);
             await dbContext.SaveChangesAsync();
 
             var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using AutoMapper;
@@ -20,10 +21,42 @@ namespace Vega.Mapping
 
             //Map Api Resource to Domain Class
             CreateMap<VehicleResource, Vehicle>()
+            .ForMember(v => v.Id, opt => opt.Ignore())
             .ForMember(v => v.ContactName, opt=> opt.MapFrom(vr=> vr.Contact.Name))
             .ForMember(v => v.ContactPhone, opt=> opt.MapFrom(vr=> vr.Contact.Phone))
             .ForMember(v => v.ContactEmail, opt=> opt.MapFrom(vr=> vr.Contact.Email))
-            .ForMember(v => v.Features, opt => opt.MapFrom(vr => vr.Features.Select(id => new VehicleFeature{FeatureId=id})));
+            .ForMember(v => v.Features, opt => opt.MapFrom(vr => vr.Features.Select(id => new VehicleFeature{FeatureId=id})))
+            .AfterMap((vr, v)=>{
+                //Remove Unselected Features
+                // var removedFeatures = new List<VehicleFeature>();
+                // foreach (var f in v.Features)
+                // {
+                //     if(!vr.Features.Contains(f.FeatureId))
+                //     removedFeatures.Add(f);
+                // }
+                var removedFeatures = v.Features.Where(v => !vr.Features.Contains(v.FeatureId));
+                foreach (var f in removedFeatures)
+                {
+                    v.Features.Remove(f);
+                }
+
+                //Add New Feature
+                // foreach (var id in vr.Features)
+                // {
+                //     if(!v.Features.Any(f => f.FeatureId == id))
+                //     {
+                //         v.Features.Add(new VehicleFeature{FeatureId = id});
+
+                //     }
+                // }
+                var newFeatures = vr.Features.Where(id => !v.Features.Any(f => f.FeatureId == id)).Select(id => new VehicleFeature{FeatureId = id});
+                foreach (var f in newFeatures)
+                {
+                    v.Features.Add(f);
+                }
+
+            });
+            ;
         }
     }
 }
